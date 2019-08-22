4e6dda1218e4b1d92b00455ab313309d24b71e1c
Jason <jzl.java@gmail.com>
2019-08-21 22:05:03 -0700

    Finished static calendar, will make new branch and start working on the dynamic feature

--- client/components/calendar.js
+++ client/components/calendar.js
@@ -7,16 +7,20 @@ class Calendar extends React.Component {
       startDay: '4',
       numberOfDays: 31,
       currentMonth: (new Date()).getMonth(),
-      currentDate: (new Date()).getDate()
+      currentDate: (new Date()).getDate(),
+      currentYear: (new Date()).getFullYear()
     }
   };
 
   render() {
+    var daysOfWeek = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
     var output = [];
     var month;
-
+    for (var i = 0; i < 7; i++) {
+      output.push(<div key={`d${i}`} className="day-box">{daysOfWeek[i]}</div>)
+    }
     for (var i = 0; i < this.data.startDay; i++) {
-      output.push(<div key={`e${i}`}className="empty-box"></div>)
+      output.push(<div key={`e${i}`} className="empty-box"></div>)
     }
     for (var i = 0; i < this.data.currentDate; i++) {
       output.push(<div key={`${i}`} className="date-box booked">{i}</div>)
@@ -26,25 +30,31 @@ class Calendar extends React.Component {
     }
 
     switch(this.data.currentMonth) {
-      case (1): month = 'Jan'; break;
-      case (2): month = 'Feb'; break;
-      case (3): month = 'Mar'; break;
-      case (4): month = 'Apr'; break;
+      case (1): month = 'January'; break;
+      case (2): month = 'Febuary'; break;
+      case (3): month = 'March'; break;
+      case (4): month = 'April'; break;
       case (5): month = 'May'; break;
-      case (6): month = 'Jun'; break;
-      case (7): month = 'Jul'; break;
-      case (8): month = 'Aug'; break;
-      case (9): month = 'Sep'; break;
-      case (10): month = 'Oct'; break;
-      case (11): month = 'Nov'; break;
-      case (12): month = 'Dec'; break;
+      case (6): month = 'June'; break;
+      case (7): month = 'July'; break;
+      case (8): month = 'August'; break;
+      case (9): month = 'September'; break;
+      case (10): month = 'October'; break;
+      case (11): month = 'November'; break;
+      case (12): month = 'December'; break;
     }
 
     return (
-      <div id="dates-container">
-        {month}
-        <div id="calendar-container">
-          {output}
+      <div id="big-container">
+        <div id="buttons-and-month">
+          <span><button>back</button></span>
+          <span id="month-name">{month} {this.data.currentYear}</span>
+          <span><button>next</button></span>
+        </div>
+        <div id="dates-container">
+          <div id="calendar-container">
+            {output}
+          </div>
         </div>
       </div>
     )
