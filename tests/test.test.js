6c4547129ce4798c874fb2dc785c636c7c08467d
Jason <jzl.java@gmail.com>
2019-08-22 16:33:31 -0700

    Attempted to fix jest, need to add babelrc still. Will do this later

--- tests/test.test.js
+++ tests/test.test.js
@@ -1,3 +1,38 @@
-it("adds correctly", () => {
-  expect(1 + 1).toEqual(2);
+import React from 'react';
+import { expect } from 'chai';
+import { render } from 'enzyme';
+
+import App from './client/components/app.js';
+
+describe('<App />', () => {
+  it('renders three `.foo-bar`s', () => {
+    const wrapper = render(<App />);
+    expect(wrapper.find('.foo-bar')).to.have.lengthOf(3);
+  });
+
+  it('renders the title', () => {
+    const wrapper = render(<Foo title="unique" />);
+    expect(wrapper.text()).to.contain('unique');
+  });
 });
+
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// // Components
+// import WelcomeMessage from './WelcomeMessage';
+
+// function setup() {
+//   const props = {
+//     imgPath: 'some/image/path/to/a/mock/image',
+//   };
+//   const wrapper = shallow(<WelcomeMessage />);
+//   return { wrapper, props };
+// }
+
+// describe('WelcomeMessage Test Suite', () => {
+//   it('Should have an image', () => {
+//     const { wrapper } = setup();
+//     expect(wrapper.find('img').exists()).toBe(true);
+//   });
+// });
