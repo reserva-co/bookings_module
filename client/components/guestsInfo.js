4b75c1f12a9166bcffec8477d47305a72e89c396
Jason <jzl.java@gmail.com>
2019-08-27 19:20:40 -0700

    Fixed so there's not unreadable classNames

--- client/components/guestsInfo.js
+++ client/components/guestsInfo.js
@@ -9,11 +9,11 @@ const GuestsInfo = () => (
           <div className="guests-info-section">
             <div className="column">Adults</div>
             <div className="guests-buttons">
-              <button type="button" className="plus-minus-button" aria-busy="false"><span className="_1y1icpxd"><svg viewBox="0 0 24 24" role="img" aria-label="subtract" focusable="false"><rect height="2" rx="1" width="12" x="6" y="11" /></svg></span></button>
+              <button type="button" className="plus-minus-button"><span><svg viewBox="0 0 24 24" role="img" aria-label="subtract"><rect height="2" rx="1" width="12" x="6" y="11" /></svg></span></button>
               <span className="num-guests">1</span>
-              <button className="plus-minus-button" type="button" aria-busy="false">
-                <span className="_1y1icpxd">
-                  <svg viewBox="0 0 24 24" role="img" aria-label="add" focusable="false">
+              <button className="plus-minus-button" type="button">
+                <span>
+                  <svg viewBox="0 0 24 24" role="img">
                     <rect height="2" rx="1" width="12" x="6" y="11" />
                     <rect height="12" rx="1" width="2" x="11" y="6" />
                   </svg>
@@ -27,11 +27,11 @@ const GuestsInfo = () => (
               <span className="subtitle description">Ages 2-12</span>
             </div>
             <div className="guests-buttons">
-              <button type="button" className="plus-minus-button" aria-busy="false"><span className="_1y1icpxd"><svg viewBox="0 0 24 24" role="img" aria-label="subtract" focusable="false"><rect height="2" rx="1" width="12" x="6" y="11" /></svg></span></button>
+              <button type="button" className="plus-minus-button"><span><svg viewBox="0 0 24 24" role="img" aria-label="subtract"><rect height="2" rx="1" width="12" x="6" y="11" /></svg></span></button>
               <span className="num-guests">1</span>
-              <button className="plus-minus-button" type="button" aria-busy="false">
-                <span className="_1y1icpxd">
-                  <svg viewBox="0 0 24 24" role="img" aria-label="add" focusable="false">
+              <button className="plus-minus-button" type="button">
+                <span>
+                  <svg viewBox="0 0 24 24" role="img">
                     <rect height="2" rx="1" width="12" x="6" y="11" />
                     <rect height="12" rx="1" width="2" x="11" y="6" />
                   </svg>
@@ -45,11 +45,11 @@ const GuestsInfo = () => (
               <span className="subtitle description">Under 2</span>
             </div>
             <div className="guests-buttons">
-              <button type="button" className="plus-minus-button" aria-busy="false"><span className="_1y1icpxd"><svg viewBox="0 0 24 24" role="img" aria-label="subtract" focusable="false"><rect height="2" rx="1" width="12" x="6" y="11" /></svg></span></button>
+              <button type="button" className="plus-minus-button"><span><svg viewBox="0 0 24 24" role="img" aria-label="subtract"><rect height="2" rx="1" width="12" x="6" y="11" /></svg></span></button>
               <span className="num-guests">1</span>
-              <button className="plus-minus-button" type="button" aria-busy="false">
-                <span className="_1y1icpxd">
-                  <svg viewBox="0 0 24 24" role="img" aria-label="add" focusable="false">
+              <button className="plus-minus-button" type="button">
+                <span>
+                  <svg viewBox="0 0 24 24" role="img">
                     <rect height="2" rx="1" width="12" x="6" y="11" />
                     <rect height="12" rx="1" width="2" x="11" y="6" />
                   </svg>
